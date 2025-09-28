307b474dc975fc9860f460ba3e21c11f8d3260a1
Steven Woolgar <steven_woolgar@woolsoft.com>
2025-09-27 21:07:39 -0700

    Shrinking the header comments

--- Examples/macOS/AppDelegate.swift
+++ Examples/macOS/AppDelegate.swift
@@ -30,6 +30,5 @@ import Cocoa
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 	func applicationDidFinishLaunching(_ aNotification: Notification) {
-		// Insert code here to initialize your application
 	}
 }
--- Examples/macOS/ViewController.swift
+++ Examples/macOS/ViewController.swift
@@ -30,6 +30,5 @@ import Cocoa
 class ViewController: NSViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
 	}
 }
